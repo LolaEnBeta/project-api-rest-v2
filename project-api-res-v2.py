5d06b5f77355e6198e90c46620a4949fce3fc87a
LolaEnBeta <lolitaenbeta@gmail.com>
2019-06-05 15:37:05 +0200

    Create a route to delete a project

--- project-api-res-v2.py
+++ project-api-res-v2.py
@@ -6,11 +6,13 @@ project_counter_id = 2
 projects = [
     {
         "name": "Project one",
-        "id": 1
+        "id": 1,
+        "tasks": []
     },
     {
         "name": "Project two",
-        "id": 2
+        "id": 2,
+        "tasks": []
     }
 ]
 
@@ -56,6 +58,14 @@ def modify_project_by_id(id):
         if project["id"] == id:
             project["name"] = request.json.get("name", project["name"])
             return jsonify({"project_modify": project})
+    abort(404)
+
+@app.route("/projects/<int:id>", methods=["DELETE"])
+def delete_project_by_id(id):
+    for project in projects:
+        if project["id"] == id:
+            projects.remove(project)
+            return jsonify({"project": "Deleted"})
     abort(404)
 
 if __name__ == "__main__":
